73d6fe24a0515534b904d5515dc2f72a37300de6
Gabor Balogh <gabor.balogh1@gmail.com>
2022-06-13 20:32:47 +0100

    update file

--- provider.tf
+++ provider.tf
@@ -1,15 +1,34 @@
+# describe deployment in this file
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "3.67.0"
+      version = "~>4.17.1"
+    }
+    random = {
+      version = "~> 2.3"
+    }
+    template = {
+      version = "~> 2.1"
+    }
+  }
+}
+
+
 # AWS provider
 provider "aws" {
   region  = var.region
-  version = "~> 2.68"
+
+  # assume_role {
+  #   role_arn    = "arn:aws:iam::889604409053:role/ENV0_Admin_Role"
+  # }
 }
 
 # Template provider
 provider "template" {
-  version = "~> 2.1"
 }
 
 # Random generator provider
 provider "random" {
-  version = "~> 2.3"
 }
